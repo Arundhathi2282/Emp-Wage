e0a90e237d60d00bcb8a5d737de90ad059ea9626
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-08-01 11:06:41 +0530

    Add : Adding part time Employee and Wage

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -6,12 +6,16 @@ public class EmployeeWage {
 	public static void main(String args[]) {
 		Random random = new Random();
 		int attendanceCheck = random.nextInt(3);
-		final int IS_PRESENT = 1;
+		final int IS_FULL_TIME_PRESENT = 1;
+		final int IS_PART_TIME_PRESENT = 2;
 		final int WAGE_PER_HR = 20;
 		int workingHrs;
-		if(attendanceCheck == IS_PRESENT){
-		    System.out.print("Employee is Present");
+		if(attendanceCheck == IS_FULL_TIME_PRESENT){
+		    System.out.print("Employee is Full Time Present");
 		    workingHrs = 8;
+		}else if (attendanceCheck == IS_PART_TIME_PRESENT){
+		    System.out.print("Employee is Part time Present");
+		    workingHrs = 4;
 		}else {
 		    System.out.print("Employee is Absent");
 		    workingHrs = 0;
