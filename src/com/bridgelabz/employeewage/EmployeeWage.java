b5712951b3ac31f806b9ed9d2fbfe46718ea6197
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-08-01 11:13:25 +0530

    Add : Solving using Switch Case

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -3,22 +3,20 @@ package com.bridgelabz.employeewage;
 import java.util.Random;
 
 public class EmployeeWage {
+	private static final int WAGE_PER_HR = 20;
 	public static void main(String args[]) {
 		Random random = new Random();
-		int attendanceCheck = random.nextInt(3);
-		final int IS_FULL_TIME_PRESENT = 1;
-		final int IS_PART_TIME_PRESENT = 2;
-		final int WAGE_PER_HR = 20;
 		int workingHrs;
-		if(attendanceCheck == IS_FULL_TIME_PRESENT){
-		    System.out.print("Employee is Full Time Present");
-		    workingHrs = 8;
-		}else if (attendanceCheck == IS_PART_TIME_PRESENT){
-		    System.out.print("Employee is Part time Present");
-		    workingHrs = 4;
-		}else {
-		    System.out.print("Employee is Absent");
-		    workingHrs = 0;
+		int attendanceCheck = random.nextInt(3);
+		switch(attendanceCheck){
+	    case 1 : System.out.print("Employee is Full time Present");
+	    workingHrs = 8;
+	    break;
+	    case 2 : System.out.print("Employee is Part time Present");
+	    workingHrs = 4;
+	    break;
+	    default : System.out.print("Employee is Absent");
+	    workingHrs = 0;
 		}
 		int salary = WAGE_PER_HR * workingHrs;
 		System.out.println(" and his salary is : "+salary);
